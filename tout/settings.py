e23d8594184d00ebb991567dfd902a24f090359b
Anurag Sharma <anuragks029@gmail.com>
2020-01-17 00:28:05 +0530

    Postgresql setup

--- tout/settings.py
+++ tout/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'tout.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tout',
+        'USER': 'tom',
+        'PASSWORD': 'godspeed',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
